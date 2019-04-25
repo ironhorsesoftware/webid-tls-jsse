4f878eb86a3e8e73022b59bc5f3a54d4b12b066e
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-04-25 16:51:02 -0400

    Fields and toString() method for the builder.

--- src/main/java/com/ironhorsesoftware/jsse/webid/certs/WebIdX509CertificateBuilder.java
+++ src/main/java/com/ironhorsesoftware/jsse/webid/certs/WebIdX509CertificateBuilder.java
@@ -14,7 +14,16 @@
  */
 package com.ironhorsesoftware.jsse.webid.certs;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.Base64;
+import java.util.List;
+
+import javax.security.auth.x500.X500Principal;
+
 import org.bouncycastle.mozilla.jcajce.JcaSignedPublicKeyAndChallenge;
+
+import com.ironhorsesoftware.jsse.webid.tls.Constants;
 
 /**
  * This class is used to construct self-signed WebID X.509 certificates.
@@ -22,6 +31,45 @@ import org.bouncycastle.mozilla.jcajce.JcaSignedPublicKeyAndChallenge;
  * @author Mike Pigott (mpigott@ironhorsesoftware.com)
  */
 public final class WebIdX509CertificateBuilder {
-  private JcaSignedPublicKeyAndChallenge pbkac;
+  private X500Principal issuer;
+  private String commonName;
+  private JcaSignedPublicKeyAndChallenge spkac;
+  private List<URI> webIds;
+  private int yearsValid;
 
+  public WebIdX509CertificateBuilder() {
+    issuer = Constants.WEBID_ISSUER;
+    commonName = null;
+    spkac = null;
+    webIds = new java.util.ArrayList<URI>();
+    yearsValid = 5;
+  }
+
+  public String toString() {
+    final String nl = System.getProperty("line.separator");
+
+    StringBuilder builder = new StringBuilder("WebIdX509CertificateBuilder");
+    builder.append(nl).append("\t     Issuer: ").append(issuer);
+    builder.append(nl).append("\tCommon Name: ").append(commonName);
+    builder.append(nl).append("\tYears Valid: ").append(yearsValid);
+
+    builder.append(nl).append("\t    Web IDs: ");
+    for (URI webId : webIds) {
+      builder.append(nl).append("\t\t").append(webId);
+    }
+
+    builder.append(nl).append("\t      SPKAC: ");
+    if (spkac != null) {
+      builder.append(nl).append("\t\tChallenge: ").append(spkac.getChallenge());
+
+      try {
+        final String encodedSpkac = Base64.getEncoder().encodeToString(spkac.getEncoded());
+        builder.append(nl).append("\t\t  SPKAC: ").append(encodedSpkac);
+      } catch (IOException e) {
+        builder.append(nl).append("\t\tSPKAC Encoding Failure: ").append(e.getMessage());
+      }
+    }
+
+    return builder.toString();
+  }
 }
